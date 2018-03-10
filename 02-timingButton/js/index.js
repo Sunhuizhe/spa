aa876f80192a323f734c50916396406b9fa528c4
Sunhuizhe <674277449@qq.com>
2018-03-10 14:38:26 +0800

    timingButton

--- 02-timingButton/js/index.js
+++ 02-timingButton/js/index.js
@@ -1,8 +1,11 @@
-var num = 9;
+var  num = 9;
+var  btn = document.getElementById('button');
 var id = setInterval(function(){
   num--;
-  $('button').val('同意(' + num + ')');
-    if(num == 0){
-      clearInterval(id);
-    }
-})
+  btn.innerHTML = '同意(' + num +'s)';
+  if(num == 0){
+    clearInterval(id);
+    btn.innerHTML = '同意';
+    btn.removeAttribute('disabled');
+  }
+},1000);
